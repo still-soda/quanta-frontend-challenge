6f6162d33bed5474a98c9b56c67e4473ee9d8ddf
still_soda <13863827+still-soda@user.noreply.gitee.com>
2025-01-06 15:34:21 +0800

    feat：添加动作、挑战、提交和通知的实体及其 DTO，完善模块结构

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ChallengesModule } from './challenges/challenges.module';
+import { ActionsModule } from './actions/actions.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { SubmissionsModule } from './submissions/submissions.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ChallengesModule,
+    ActionsModule,
+    NotificationsModule,
+    SubmissionsModule,
+  ],
   controllers: [],
   providers: [],
 })
 export class AppModule {}
-6;
--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Swagger
   const config = new DocumentBuilder()
