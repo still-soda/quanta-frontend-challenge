1c9c6410231a7a6ff8435c8d9bb917fa6803d23a
still_soda <951040628@qq.com>
2025-01-10 16:21:22 +0800

    feat：更新 Jest 版本，添加数据结构验证器及其测试用例，重构 tsconfig 配置

---
+++ utils/src/__tests__/data-structure-validator.spec.ts
@@ -0,0 +1,86 @@
+import { contain, fit } from '../data-structure-validator';
+
+describe('数据验证器 - fn[container]', () => {
+   it('应该正确验证包含关系', () => {
+      const obj = { hello: 12, world: 20 };
+      expect(contain(obj, ['hello', 'world'])).toBe(true);
+   });
+
+   it('应该正确验证不包含关系', () => {
+      const obj = { hello: 12, world: 20 };
+      expect(contain(obj, ['hello', 'world', '12'] as any)).toBe(true);
+   });
+
+   it('当传入数据为 undefined 或 null 的时候应该返回 false', () => {
+      expect(contain(null as any, [])).toBe(false);
+      expect(contain(undefined as any, [])).toBe(false);
+   });
+});
+
+describe('数据验证器 - fn[fit]', () => {
+   it('应该正确验证正确的类型', () => {
+      const obj = {
+         a: 12,
+         b: 'hello',
+         c: false,
+         d: 'click',
+         e: 'move',
+         f: [],
+         g: {
+            a: 123,
+            b: 'hello',
+            c: {
+               a: 123,
+            },
+         },
+         h: 'str',
+      };
+      const dataStructure = {
+         a: 'number',
+         b: 'string',
+         c: 'boolean',
+         d: ['click', 'move'],
+         e: (val: string) => val === 'move',
+         f: Array.isArray,
+         g: {
+            a: 'number',
+            b: 'string',
+            c: {
+               a: 'number',
+            },
+         },
+         h: 'str',
+      };
+
+      const result = fit(obj, dataStructure);
+      expect(result.ok).toBe(true);
+   });
+
+   it('应该正确验证错误的类型（第 1 层）', () => {
+      const obj = { a: 123, b: 'hello' };
+      expect(fit(obj, { a: 'string', b: 'number' }).ok).toBe(false);
+   });
+
+   it('应该正确验证错误的类型（第 n 层）', () => {
+      const obj = { a: { b: 123 } };
+      const result = fit(obj, { a: { b: 'string' } });
+      expect(result.ok).toBe(false);
+   });
+
+   it('应该正确验证缺少相应类型的情况（第 1 层）', () => {
+      const obj = { a: 12, b: 20 };
+      expect(fit(obj, { a: 'number', b: 'number', c: 'string' }).ok).toBe(
+         false
+      );
+   });
+
+   it('应该正确验证缺少相应类型的情况（第 n 层）', () => {
+      const obj = { a: 12, b: 20, c: { a: 20 } };
+      const fitResult = fit(obj, {
+         a: 'number',
+         b: 'number',
+         c: { a: 'numebr', b: 'string' },
+      });
+      expect(fitResult.ok).toBe(false);
+   });
+});
---
+++ utils/src/data-structure-validator.ts
@@ -0,0 +1,66 @@
+export function contain<T extends Record<string, any>>(
+   obj: T,
+   keys: Array<keyof T>
+) {
+   if (!obj || (obj as any).__checked__) {
+      return false;
+   }
+   (obj as any).__checked__ = true;
+
+   keys.forEach((key) => {
+      if (!obj[key]) return false;
+   });
+   return true;
+}
+
+const BASIC_TYPES = [
+   'string',
+   'object',
+   'number',
+   'function',
+   'bigint',
+   'symbol',
+   'boolean',
+   'undefined',
+] as const;
+const BASIC_TYPES_SET = new Set(BASIC_TYPES);
+
+export function fit(
+   obj: Record<string, any>,
+   dataStructure: Record<string, typeof BASIC_TYPES | any>
+): { msg: string; ok: boolean } {
+   const entries = Object.entries(dataStructure);
+   for (const [key, val] of entries) {
+      if (val !== 'undefined' && obj[key] === undefined) {
+         return { msg: `不存在${key}`, ok: false };
+      } else if (BASIC_TYPES_SET.has(val)) {
+         if (typeof obj[key] !== val) {
+            return { msg: `${key} 类型错误`, ok: false };
+         }
+      } else if (typeof val === 'function') {
+         if (!val(obj[key])) {
+            return { msg: `对于 ${key} 的函数验证不通过`, ok: false };
+         }
+      } else if (Array.isArray(val)) {
+         if (!val.includes(obj[key])) {
+            return {
+               msg: `k[${key}]:v[${obj[key]}] 不在可行列表中, li[${val}]`,
+               ok: false,
+            };
+         }
+      } else if (typeof val === 'string') {
+         if (obj[key] !== val) {
+            return {
+               msg: `字符串 k[${key}]:v[${obj[key]}] !== ${val}`,
+               ok: false,
+            };
+         }
+      } else if (typeof val === 'object') {
+         const result = fit(obj[key], val);
+         if (!result.ok) {
+            return result;
+         }
+      }
+   }
+   return { msg: 'ok', ok: true };
+}
