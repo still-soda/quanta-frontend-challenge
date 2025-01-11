addfa450c23e4b79a1bd1af4bd9c2b7fed4963df
still_soda <951040628@qq.com>
2025-01-11 15:30:43 +0800

    feat：移除不必要的 flow-data.validator.ts 文件，简化鼠标流数据验证器的实现

--- packages/backend/src/modules/tasks/core/flow-data-types/actions/mouse-flow-data.validator.ts
+++ packages/backend/src/modules/tasks/core/flow-data-types/actions/mouse-flow-data.validator.ts
@@ -1,18 +1,5 @@
-import {
-  MouseFlowData,
-  MoveMouseFlowData,
-  ClickMouseFlowData,
-} from './mouse-flow-data.type';
+import { MoveMouseFlowData, ClickMouseFlowData } from './mouse-flow-data.type';
 import { fit, Optional } from '@challenge/utils';
-
-export function validateMouseFlowData(data: MouseFlowData) {
-  return fit(data, {
-    type: 'mouse',
-    detail: {
-      type: ['move', 'click'],
-    },
-  });
-}
 
 export function validateMoveMouseFlowData(data: MoveMouseFlowData) {
   return fit(data, {
