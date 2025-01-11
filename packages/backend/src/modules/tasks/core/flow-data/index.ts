98a3415e62f53811e0aa6ed688f56a7a46b3bce3
still_soda <951040628@qq.com>
2025-01-11 22:41:11 +0800

    feat：添加触发器数据验证器，增强流程数据验证能力；更新相关导出

--- packages/backend/src/modules/tasks/core/flow-data/index.ts
+++ packages/backend/src/modules/tasks/core/flow-data/index.ts
@@ -1,5 +1,6 @@
 // VALIDATORS
 import * as MouseFlowDataValidators from './actions/mouse-flow-data.validator';
+import * as TriggerFlowDataValidators from './actions/trigger-flow-data.validator';
 import { FlowData } from './flow-data.type';
 import * as TestpointsFlowDataValidators from './testpoints/testpoints-flow-data.validator';
 
@@ -7,14 +8,22 @@ export const dataValidators = [
   MouseFlowDataValidators.validateClickMouseFlowData,
   MouseFlowDataValidators.validateMoveMouseFlowData,
   MouseFlowDataValidators.validateScrollMouseFlowData,
+  TriggerFlowDataValidators.validateBlurTriggerFlowData,
+  TriggerFlowDataValidators.validateDragTriggerFlowData,
+  TriggerFlowDataValidators.validateFocusTriggerFlowData,
+  TriggerFlowDataValidators.validateHoverTriggerFlowData,
+  TriggerFlowDataValidators.validateInputTriggerFlowData,
+  TriggerFlowDataValidators.validateWaitTriggerFlowData,
   TestpointsFlowDataValidators.validateExpectTestpointFlowData,
   TestpointsFlowDataValidators.validateScreenShotTestpointFlowData,
 ];
 export * from './actions/mouse-flow-data.validator';
+export * from './actions/trigger-flow-data.validator';
 export * from './testpoints/testpoints-flow-data.validator';
 
 // TYPES
 export type Validator = (data: FlowData) => { ok: boolean; msg: string };
 export * from './actions/mouse-flow-data.type';
+export * from './actions/trigger-flow-data.type';
 export * from './testpoints/testpoints-flow-data.type';
 export * from './flow-data.type';
