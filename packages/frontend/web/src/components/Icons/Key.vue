1111c7b723d6dac6320a4b81c9ef1f8c3ad0e525
still_soda <951040628@qq.com>
2025-03-13 21:29:51 +0800

    feat：添加多个图标组件，包括身份证、钥匙、邮件、机器人和用户

---
+++ packages/frontend/web/src/components/Icons/Key.vue
@@ -0,0 +1,33 @@
+<template>
+   <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M7.5 20.5C9.70914 20.5 11.5 18.7091 11.5 16.5C11.5 14.2909 9.70914 12.5 7.5 12.5C5.29086 12.5 3.5 14.2909 3.5 16.5C3.5 18.7091 5.29086 20.5 7.5 20.5Z"
+         stroke="currentColor"
+         stroke-width="2" />
+      <path
+         d="M14.5 8L17.75 11"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round" />
+      <path
+         d="M10 13L18.5 3.5"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round" />
+      <path
+         d="M17.5 5.5L21 8.75"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round" />
+   </svg>
+</template>
+
+<script setup lang="ts"></script>
---
+++ packages/frontend/web/src/components/Icons/Mail.vue
@@ -0,0 +1,34 @@
+<template>
+   <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M2 19.5H22V12V4.5H12H2V12V19.5Z"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linejoin="round" />
+      <path
+         d="M2 4.5L12 12L22 4.5"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round" />
+      <path
+         d="M12 4.5H2V12"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round" />
+      <path
+         d="M22 12V4.5H12"
+         stroke="currentColor"
+         stroke-width="2"
+         stroke-linecap="round"
+         stroke-linejoin="round" />
+   </svg>
+</template>
+
+<script setup lang="ts"></script>
