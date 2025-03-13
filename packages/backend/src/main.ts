6cc8e614ae83580a7c8a73a894e558d1f1af7ccf
still_soda <951040628@qq.com>
2025-03-13 23:17:30 +0800

    feat：移除主文件中的 CORS 支持

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -12,8 +12,6 @@ async function bootstrap() {
   });
   app.useGlobalFilters(new GlobalExceptionFilter());
 
-  app.enableCors();
-
   // Swagger
   const config = new DocumentBuilder()
     .setTitle('Quanta Frontend Challenge Web API')
