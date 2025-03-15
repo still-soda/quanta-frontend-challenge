41b05ccda55ec00644e4653a426c6189edf79853
still_soda <951040628@qq.com>
2025-03-15 11:02:23 +0800

    feat：添加默认头像的Base64常量

---
+++ packages/frontend/web/src/constant/default.constant.ts
@@ -0,0 +1,5 @@
+/**
+ * 默认头像Base64
+ */
+export const DEFAULT_AVATAR =
+   'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMzEgMjMxIiBmaWxsPSIjRjVGN0ZCIj48L3N2Zz4=';
