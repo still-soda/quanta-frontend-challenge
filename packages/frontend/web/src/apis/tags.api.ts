bdc936b15abbeaa5b02536e6a8453aa28a87b44c
still_soda <951040628@qq.com>
2025-03-16 11:31:37 +0800

    feat：添加标签API，支持获取所有标签、根据ID获取标签及根据多个ID返回标签列表

---
+++ packages/frontend/web/src/apis/tags.api.ts
@@ -0,0 +1,31 @@
+import { Tag } from '@/models/tag.model';
+import { RequestResult } from '@/types/request';
+import { get } from '@challenge/api';
+
+/**
+ * 获取所有标签
+ * @returns 所有标签
+ */
+export async function getAllTags() {
+   return get<RequestResult<Tag[]>>('/tags/find-all');
+}
+
+/**
+ * 根据ID获取标签
+ * @param id 标签ID
+ * @return 单个标签
+ */
+export async function getTagById(id: string) {
+   return get<RequestResult<Tag>>(`/tags/find-one/${id}`);
+}
+
+/**
+ * 根据多个 ID 返回标签列表
+ * @param idList ID 列表
+ * @returns 标签列表
+ */
+export async function findByIds(idList: string[]) {
+   return get<RequestResult<Tag[]>>('/tags/find-by-ids', {
+      body: JSON.stringify({ id: idList }),
+   });
+}
