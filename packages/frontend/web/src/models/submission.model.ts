08d16347ff6cca031e1d43554bf02f290cc65cf4
still_soda <951040628@qq.com>
2025-03-18 21:15:00 +0800

    feat：更新提交模型，添加提交ID、解决方案和截图ID属性

--- packages/frontend/web/src/models/submission.model.ts
+++ packages/frontend/web/src/models/submission.model.ts
@@ -5,6 +5,7 @@ export type SubmissionStatus = 'pending' | 'passed' | 'failed';
 
 /**
  * 提交数据
+ * @property _id - 提交ID
  * @property challengeId - 挑战ID
  * @property userId - 用户ID
  * @property type - 类型
@@ -12,10 +13,13 @@ export type SubmissionStatus = 'pending' | 'passed' | 'failed';
  * @property correctRate - 正确率
  * @property status - 状态
  * @property message - 消息
+ * @property solution - 解决方案
+ * @property screenshotIds - 截图ID
  * @property createdAt - 创建时间
  * @property updatedAt - 更新时间
  */
 export type Submission = {
+   _id: string;
    challengeId: string;
    userId: string;
    type: string;
@@ -23,6 +27,8 @@ export type Submission = {
    correctRate: number;
    status: SubmissionStatus;
    message: string;
+   screenshotIds: string[];
+   solution: number;
    createdAt: string;
    updatedAt: string;
 };
