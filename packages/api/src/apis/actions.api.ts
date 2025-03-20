e7ada1acba06472e5bb22c0600b3f10873678a34
still_soda <951040628@qq.com>
2025-03-20 23:03:37 +0800

    feat：重构动作API，使用枚举定义API路径

--- packages/api/src/apis/actions.api.ts
+++ packages/api/src/apis/actions.api.ts
@@ -2,17 +2,28 @@ import { Action } from '../models';
 import { RequestResult } from '../utils/request.types';
 import { get } from '../utils/request.utils';
 
+export enum ActionApi {
+   GET_MY_ACTIONS = '/actions/my-action',
+   GET_ACTION_BY_ID = '/actions/one-action',
+}
+
 /**
  * 获取我的动态
  * @api /actions/my-action
  * @returns 我的动态
  */
 export async function getMyActions() {
-   return get<RequestResult<Action[]>>('/actions/my-action');
+   return get<RequestResult<Action[]>>(ActionApi.GET_MY_ACTIONS);
 }
 
+/**
+ * 获取动态详情
+ * @api /actions/one-action
+ * @param id 动态 id
+ * @returns 动态详情
+ */
 export async function getActionById(id: string) {
-   return get<RequestResult<Action>>(`/actions/one-action`, {
+   return get<RequestResult<Action>>(ActionApi.GET_ACTION_BY_ID, {
       query: { id },
    });
 }
