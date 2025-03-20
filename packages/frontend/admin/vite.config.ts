f27b5700a4aab22e8e3224d99b9a85e286797044
still_soda <951040628@qq.com>
2025-03-20 17:25:10 +0800

    feat：添加环境变量配置，更新Vite配置以支持JSX和API代理

--- packages/frontend/admin/vite.config.ts
+++ packages/frontend/admin/vite.config.ts
@@ -4,10 +4,24 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [vue() as any],
+   plugins: [vue() as any, '@vue/babel-plugin-jsx'],
    resolve: {
       alias: {
          '@': resolve(__dirname, './src'),
       },
+   },
+   server: {
+      proxy: {
+         '/api': {
+            target: 'http://localhost:3000',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+         },
+      },
+   },
+   esbuild: {
+      jsxFactory: 'h',
+      jsxFragment: 'Fragment',
+      jsxInject: `import { h, Fragment } from 'vue'`,
    },
 });
