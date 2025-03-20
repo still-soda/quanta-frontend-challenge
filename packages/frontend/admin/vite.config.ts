8e000a9d1a6671b9618d7df830152493ba4af920
still_soda <951040628@qq.com>
2025-03-20 23:10:44 +0800

    feat：在前端管理系统中添加mock模块支持，并更新相关依赖

--- packages/frontend/admin/vite.config.ts
+++ packages/frontend/admin/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteMockServe } from 'vite-plugin-mock';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [vue() as any, '@vue/babel-plugin-jsx'],
+   plugins: [
+      vue() as any,
+      '@vue/babel-plugin-jsx',
+      viteMockServe({
+         mockPath: 'mock',
+         enable: true,
+      }),
+   ],
    resolve: {
       alias: {
          '@': resolve(__dirname, './src'),
