23fab59326225252e87bcf5ff0c8cd089de4d347
still_soda <951040628@qq.com>
2025-03-28 21:07:40 +0800

    feat: 添加文档解析功能，支持自定义扩展键

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -6,3 +6,4 @@ export * from './src/event-emitter';
 export * from './src/range';
 export * from './src/data-structure-validator/index';
 export * from './src/svg-to-base64';
+export * from './src/resolve-doc';
