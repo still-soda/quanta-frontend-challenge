c23e9cc24d822de7a7479ba907285138dc9da4be
still_soda <951040628@qq.com>
2025-04-11 18:14:31 +0800

    feat: 添加挑战难度常量并修正标签颜色和文本映射中的拼写错误

---
+++ packages/frontend/admin/src/constant/select.constant.ts
@@ -0,0 +1,8 @@
+/**
+ * 挑战难度
+ */
+export enum DIFFICULTY {
+   EASY = 'easy',
+   MEDIUM = 'medium',
+   HARD = 'hard',
+}
--- packages/frontend/admin/src/constant/tags.constant.ts
+++ packages/frontend/admin/src/constant/tags.constant.ts
@@ -10,15 +10,15 @@ export const TAG_COLOR_MAPPING: Readonly<Record<string, string>> =
       // 语言
       css: '#60A5FA',
       javascript: '#FF9232',
-      html: '#FF9232',
+      html: '#FF6565',
       typescript: '#60A5FA',
       vue: '#94B889',
       react: '#60A5FA',
 
       // 难度
-      eazy: '#60A5FA',
+      easy: '#60A5FA',
       medium: '#FF9232',
-      hard: '#FF3332',
+      hard: '#FF6565',
    });
 
 /**
@@ -40,8 +40,36 @@ export const TAG_TEXT_MAPPING: Readonly<Record<string, string>> = Object.freeze(
       react: 'React',
 
       // 难度
-      eazy: '简单',
+      easy: '简单',
       medium: '中等',
       hard: '困难',
    }
 );
+
+/**
+ * 状态颜色映射
+ * @example
+ * ```ts
+ * STATUS_COLOR_MAPPING[0] // '#FF9232'
+ * ```
+ */
+export const STATUS_COLOR_MAPPING: Readonly<string[]> = Object.freeze([
+   '#FF9232',
+   '#60A5FA',
+   '#94B889',
+   '#FF6565',
+]);
+
+/**
+ * 状态文本映射
+ * @example
+ * ```ts
+ * STATUS_TEXT_MAPPING[0] // '草稿中'
+ * ```
+ */
+export const STATUS_TEXT_MAPPING: Readonly<string[]> = Object.freeze([
+   '草稿中',
+   '准备中',
+   '已发布',
+   '已关闭',
+]);
