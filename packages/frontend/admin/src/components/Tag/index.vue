421504239312581e5fa7743b00964eb9e6f87295
still_soda <951040628@qq.com>
2025-04-11 18:15:32 +0800

    feat: 添加标签组件和更新布局，优化菜单结构和样式

---
+++ packages/frontend/admin/src/components/Tag/index.vue
@@ -0,0 +1,9 @@
+<template>
+   <TTag size="small" :style="{ backgroundColor: color, color: '#fff' }">
+      <slot></slot>
+   </TTag>
+</template>
+
+<script setup lang="ts">
+defineProps<{ color: string }>();
+</script>
--- packages/frontend/admin/src/views/app/layout.vue
+++ packages/frontend/admin/src/views/app/layout.vue
@@ -1,86 +1,217 @@
 <template>
    <TLayout class="h-screen">
-      <TAside>
+      <TAside class="w-fit shrink-0 overflow-hidden">
          <TMenu
             theme="light"
-            value="dashboard"
-            style="margin-right: 50px"
-            height="550px">
+            :value="activeMenu"
+            style="margin-right: 0px"
+            expand-mutex
+            class="w-fit"
+            height="550px"
+            :collapsed="collapsed"
+            @change="handleChange">
             <template #logo>
                <img
-                  width="136"
-                  class="logo"
-                  src="https://www.tencent.com/img/index/menu_logo_hover.png"
+                  width="32"
+                  class="rounded-full"
+                  src="@/assets/imgs/quanta-logo.jpg"
                   alt="logo" />
+               <h1
+                  class="ml-2 font-semibold text-orange-high transition duration-300"
+                  :class="{
+                     'opacity-0 pointer-events-none': collapsed,
+                  }">
+                  Quanta前端挑战后台
+               </h1>
             </template>
-            <TMenuItem value="dashboard">
-               <template #icon>
-                  <TIcon name="dashboard" />
-               </template>
+
+            <TMenuItem :value="AppRoute.DASHBOARD">
+               <template #icon><TIcon name="dashboard" /></template>
                仪表盘
             </TMenuItem>
-            <TMenuItem value="resource">
+
+            <TSubmenu value="challenge" title="挑战">
+               <template #icon><TIcon name="assignment-code" /></template>
+               <TMenuItem :value="AppRoute.CHALLENGE_MANAGE">
+                  <template #icon><TIcon name="list" /></template>
+                  管理挑战
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.CHALLENGE_SUBMIT">
+                  <template #icon><TIcon name="analytics" /></template>
+                  作答情况
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.CHALLENGE_PUBLISH">
+                  <template #icon><TIcon name="upload" /></template>
+                  发布挑战
+               </TMenuItem>
+            </TSubmenu>
+
+            <TSubmenu value="tag" title="标签">
                <template #icon>
-                  <TIcon name="server" />
+                  <TIcon name="tag" />
                </template>
-               资源列表
+               <TMenuItem :value="AppRoute.TAG_MANAGE">
+                  <template #icon><TIcon name="list" /></template>
+                  管理标签
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.TAG_SEARCH">
+                  <template #icon><TIcon name="search" /></template>
+                  标签查询
+               </TMenuItem>
+            </TSubmenu>
+
+            <TSubmenu value="article" title="公告">
+               <template #icon><TIcon name="article" /></template>
+               <TMenuItem :value="AppRoute.NOTIFICATION_MANAGE">
+                  <template #icon><TIcon name="list" /></template>
+                  管理公告
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.NOTIFICATION_VIEW_DATA">
+                  <template #icon><TIcon name="analytics" /></template>
+                  浏览数据
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.NOTIFICATION_PUBLISH">
+                  <template #icon><TIcon name="upload" /></template>
+                  发布公告
+               </TMenuItem>
+            </TSubmenu>
+
+            <TSubmenu value="user" title="用户">
+               <template #icon><TIcon name="user" /></template>
+               <TMenuItem :value="AppRoute.USER_MANAGE">
+                  <template #icon><TIcon name="list" /></template>
+                  管理用户
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.USER_ACTION">
+                  <template #icon><TIcon name="activity" /></template>
+                  行为数据
+               </TMenuItem>
+               <TMenuItem :value="AppRoute.USER_RESPONSE">
+                  <template #icon><TIcon name="markup" /></template>
+                  作答情况
+               </TMenuItem>
+            </TSubmenu>
+
+            <TMenuItem :value="AppRoute.RANK">
+               <template #icon><TIcon name="leaderboard" /></template>
+               得分排名
             </TMenuItem>
-            <TMenuItem value="root">
-               <template #icon>
-                  <TIcon name="root-list" />
-               </template>
-               根目录
+
+            <TMenuItem :value="AppRoute.BROWSE">
+               <template #icon><TIcon name="accessibility" /></template>
+               访问数据
             </TMenuItem>
-            <TMenuItem value="control-platform">
-               <template #icon>
-                  <TIcon name="control-platform" />
-               </template>
-               调度平台
+
+            <TMenuItem :value="AppRoute.RESPONSE">
+               <template #icon><TIcon name="calendar-edit" /></template>
+               作答数据
             </TMenuItem>
-            <TMenuItem value="precise-monitor">
-               <template #icon>
-                  <TIcon name="precise-monitor" />
-               </template>
-               调度平台
-            </TMenuItem>
-            <TMenuItem value="mail">
-               <template #icon>
-                  <TIcon name="mail" />
-               </template>
-               消息区
-            </TMenuItem>
-            <TMenuItem value="user-circle">
-               <template #icon>
-                  <TIcon name="user-circle" />
-               </template>
-               个人中心
-            </TMenuItem>
-            <TMenuItem value="play-circle">
-               <template #icon>
-                  <TIcon name="play-circle" />
-               </template>
-               视频区
-            </TMenuItem>
-            <TMenuItem value="edit1">
-               <template #icon>
-                  <TIcon name="edit-1" />
-               </template>
-               资源编辑
-            </TMenuItem>
+
+            <template #operations>
+               <TSpace size="small" :direction="collapsed ? 'vertical' : ''">
+                  <TButton
+                     variant="text"
+                     shape="square"
+                     @click="handleCollapse">
+                     <TIcon name="menu-fold" />
+                  </TButton>
+                  <TPopconfirm
+                     :visible="logoutConfirmVisible"
+                     @visible-change="onVisibleChange"
+                     content="确认要登出吗？">
+                     <TButton variant="text" shape="square">
+                        <TIcon name="logout" />
+                     </TButton>
+                  </TPopconfirm>
+               </TSpace>
+            </template>
          </TMenu>
       </TAside>
-      <TLayout>
+
+      <TLayout class="h-screen overflow-auto">
          <TContent>
-            <div>
-               <RouterView />
-            </div>
+            <RouterView v-slot="{ Component }">
+               <div class="w-full p-4">
+                  <Transition name="page" mode="out-in">
+                     <Component :is="Component" />
+                  </Transition>
+               </div>
+            </RouterView>
          </TContent>
-         <TFooter>
-            Copyright @ 2019-{{ new Date().getFullYear() }} Tencent. All Rights
-            Reserved
-         </TFooter>
       </TLayout>
    </TLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMessage } from '@/hooks/use-message.hook';
+import { AppRoute } from '@/routes/app.route';
+import { logout } from '@challenge/api';
+import { PopconfirmProps } from 'tdesign-vue-next';
+import { ref, watchEffect } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const router = useRouter();
+const route = useRoute();
+const message = useMessage();
+
+const paths = ref<string[]>([]);
+watchEffect(() => {
+   paths.value = router.getRoutes().map((route) => route.path);
+});
+
+// 处理折叠菜单
+const collapsed = ref(false);
+function handleCollapse() {
+   collapsed.value = !collapsed.value;
+}
+
+// 处理登出
+const logoutConfirmVisible = ref(false);
+const onVisibleChange: PopconfirmProps['onVisibleChange'] = async (
+   val,
+   context
+) => {
+   if (context && context.trigger === 'confirm') {
+      await logout();
+      message.success('登出成功', { duration: 3000 });
+      router.push('/login');
+   } else {
+      logoutConfirmVisible.value = val;
+   }
+};
+
+// 处理菜单更改
+const activeMenu = ref(route.path);
+function handleChange(active: AppRoute) {
+   if (paths.value.includes(active)) {
+      router.push(active);
+      activeMenu.value = active;
+   } else {
+      message.error(`不存在路由 "${active}"`, { duration: 3000 });
+   }
+}
+</script>
+
+<style scoped>
+.page-enter-active,
+.page-leave-active {
+   transition:
+      opacity 0.5s,
+      transform 0.5s,
+      filter 0.3s;
+}
+
+.page-enter-from,
+.page-leave-to {
+   opacity: 0;
+   filter: blur(10px);
+   transform: translateY(1.25rem);
+}
+
+.page-enter-to,
+.page-leave-from {
+   opacity: 1;
+   filter: blur(0);
+   transform: translateY(0);
+}
+</style>
